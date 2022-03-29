5e90e263766db8bec01e4252dcfb6ac667bfb418
HayeonLee88 <hayeon0808@swu.ac.kr>
2022-03-29 19:49:10 +0900

    AssembleGame, ShootingGame

---
+++ PBL_01/Assets/Resources/Shooting game/Target_move.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target_move : MonoBehaviour
+{
+    float speed = 0.8f;
+    float yMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Up();
+    }
+    public void Up()
+    {
+        StartCoroutine(UpFlow());
+    }
+    public void Down()
+    {
+        StartCoroutine(DownFlow());
+    }
+    IEnumerator UpFlow()
+    {
+        while (this.transform.position.y < 1000.0f)
+        {
+            yMove = 0;
+            yMove = speed;
+            this.transform.Translate(new Vector3(0, yMove, 0));
+            yield return null;
+        }
+        StartCoroutine(DownFlow());
+        yield return null;
+    }
+    IEnumerator DownFlow()
+    {
+        while (this.transform.position.y > 500.0f)
+        {
+            yMove = 0;
+            yMove = -speed;
+            this.transform.Translate(new Vector3(0, yMove, 0));
+            yield return null;
+        }
+        StartCoroutine(UpFlow());
+        yield return null;
+    }
+}
---
+++ PBL_01/Assets/Resources/Shooting game/shooting.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class shooting : MonoBehaviour
+{
+    public Image mark;
+    public Image aim;
+    public Button btn;
+
+    bool triggered = false;
+
+    Vector2 shoot;
+
+    // Start is called before the first frame update
+    void Start() { 
+
+    }
+    private void Update()
+    {
+        triggered = false;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //Debug.Log("Ãæµ¹");
+        triggered = true;
+
+    }
+    public void Button_click()
+    {
+        if (triggered && mark.gameObject.activeSelf==false )
+        {
+            shoot = aim.gameObject.transform.position;
+            mark.transform.position = shoot;
+            mark.gameObject.SetActive(true);
+        }
+    }
+}
