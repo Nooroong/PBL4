2c36e72805421238f6e43ca10243c220148c2e8a
swucare19 <swucare19@gmail.com>
2022-05-22 14:24:25 +0900

    youngsin
    Note0_Btn : 주인공의 ps에 따라 정비사 ps지정

--- PBL_01/Assets/Note0_Btn.cs
+++ PBL_01/Assets/Note0_Btn.cs
@@ -8,10 +8,12 @@ public class Note0_Btn : MonoBehaviour
     public GameObject Note0, Note1;
     public Button btn;
     public Text text;
+    public Text ps;
 
     // Start is called before the first frame update
     void Start()
     {
+        ps.gameObject.SetActive(false);
         switch (PlayerPrefs.GetInt("day"))
         {
             case 2:
@@ -23,10 +25,23 @@ public class Note0_Btn : MonoBehaviour
                             "뉴스에 너 얘기가 나왔더라. 식탁 위에 신문 확인해봐.";
                 break;
             case 3:
+                
+                if(PlayerPrefs.GetString("note").Contains("퇴근하시고 피곤하실텐데 일찍 주무세요!"))
+                {
+                    ps.text = "낯선 곳에 와서 여러모로 힘들텐데 제대로 챙겨주지 못해 미안해.";
+                }
+                else if(PlayerPrefs.GetString("note").Contains("말썽 안 피우고 잘 지낼게요!"))
+                {
+                    ps.text = "나 없는 동안 뭐 하고 지냈는지 궁금하구나.";
+                }
+                else
+                {
+                    ps.text = "나도 볼 수 있으면 좋겠구나.";
+                }
                 text.text = "아침 일찍 나가고 밤 늦게 들어오니까 얼굴 볼 일이 적네." + "\n" +
                             "오늘 하루도 조심히 잘 지내! 여기서 지내는 동안 마음놓고 편하게 있으렴." + "\n" +
-                            "오늘도 밥 먹고 약 잊지마. 오늘 마을에 축제 열리니까 구경해봐.";
-                            
+                            "오늘도 밥 먹고 약 잊지마. 오늘 마을에 축제 열리니까 구경해봐." + "\n" +
+                            ps.text;
                 break;
         }
     }
