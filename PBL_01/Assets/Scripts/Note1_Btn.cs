f75760d4da25bc126857957c8238ef7aa551bcbd
swucare19 <swucare19@gmail.com>
2022-11-09 15:40:53 +0900

    youngsin
    Treatment씬
    - 카메라, 캔버스 수정
    Prologue_ForcedLanding씬
    - Spaceship, smoke 이미지, 애니메이션 추가
    Note씬
    - Note0_Btn, Note1_Btn 스크립트에 Day4, 5 추가
    - Day5는 추신 하나만 선택 가능 -> 주인공 추신이 길어서 btn4 만들고 btn1,2,3은 비활성시킴.
    Ending_Credit씬
    - Day4 bgm 저작권 표시 추가
    Day6_LeaveEarth씬
    - 이미지/애니메이션 추가
    Day6_BackToUnemotion03씬
    - image(10) 추가
    - Image(9)에 LightBtn 효과음 추가->무드등을 끄면서 화면이 꺼지는 효과를 줌.

--- PBL_01/Assets/Scripts/Note1_Btn.cs
+++ PBL_01/Assets/Scripts/Note1_Btn.cs
@@ -7,10 +7,10 @@ public class Note1_Btn : MonoBehaviour
 {
     public GameObject Note1, Note2;
     public Text ps;
-    public Button btn1, btn2, btn3;
+    public Button btn1, btn2, btn3, btn4;
     public Button next;
     public Text note;
-    public Text b1text, b2text, b3text;
+    public Text b1text, b2text, b3text, b4text;
 
     // Start is called before the first frame update
     void Start()
@@ -36,6 +36,26 @@ public class Note1_Btn : MonoBehaviour
                 b2text.text = "오늘은 무슨 일이 일어날지 걱정돼요.";
                 b3text.text = "오늘은 꼭 볼 수 있었으면 좋겠어요.";
                 break;
+            case 4:
+                note.text = "쪽지 잘 읽었어요. 어... 무슨 느낌이라고 해야하지..." + "\n" +
+                            "되게 부끄럽네요. 제가 아니었어도 누구든 했어야 했을 일이었을 거예요. 다른 사람에게 오랜만에 칭찬을 들어서 새롭고, 스스로 변하는 게 느껴져요." + "\n";
+
+
+                b1text.text = "내일 아침 쪽지도 기대할게요.";
+                b2text.text = "지구에서의 생활이 점점 적응되고 있어요.";
+                b3text.text = "오늘은 볼 수 있겠죠?";
+                break;
+            case 5:
+                note.text = "아침밥 감사해요! 밥 진짜 맛있었어요." + "\n" +
+                            "이제 떠날 날이 얼마 남지 않았네요. 제 얘기를 듣고 기뻤다니 저도 이 쪽지를 보면서 행복해요. 마지막까지 잘 지낼게요." + "\n";
+
+                btn1.gameObject.SetActive(false);
+                btn2.gameObject.SetActive(false);
+                btn3.gameObject.SetActive(false);
+                btn4.gameObject.SetActive(true);
+                b4text.text = "지금까지 잘 챙겨주셔서 감사했습니다. 나중에 만나게 되면 제가 음식 해드릴게요!";
+
+                break;
         }
 
 
@@ -51,8 +71,6 @@ public class Note1_Btn : MonoBehaviour
         ps.text ="ps. " + b1text.text;
         PlayerPrefs.SetString("note", (note.text+"\n"+ps.text));
         next.interactable = true;
-
-
     }
     public void Btn2()
     {
@@ -63,6 +81,12 @@ public class Note1_Btn : MonoBehaviour
     public void Btn3()
     {
         ps.text = "ps. " + b3text.text;
+        PlayerPrefs.SetString("note", (note.text + "\n" + ps.text));
+        next.interactable = true;
+    }
+    public void Btn4()
+    {
+        ps.text = "ps. " + b4text.text;
         PlayerPrefs.SetString("note", (note.text + "\n" + ps.text));
         next.interactable = true;
     }
